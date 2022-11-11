268b4e10329ab72074147f3eca37481e2c4012c5
park <mcpc0816@naver.com>
2022-11-11 17:01:26 +0900

    ch17-7

--- app_public/src/app/app.module.ts
+++ app_public/src/app/app.module.ts
@@ -6,16 +6,39 @@ import { HomeListComponent } from './home-list/home-list.component';
 import { DistancePipe } from './distance.pipe';
 import {HttpClientModule} from "@angular/common/http";
 
+import {RouterModule} from '@angular/router';
+import { FrameworkComponent } from './framework/framework.component';
+import { AboutComponent } from './about/about.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { PageHeaderComponent } from './page-header/page-header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
 @NgModule({
   declarations: [
     HomeListComponent,
-    DistancePipe
+    DistancePipe,
+    FrameworkComponent,
+    AboutComponent,
+    HomepageComponent,
+    PageHeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {
+        path:'',
+        component:HomepageComponent
+      },
+      {
+        path:'about',
+        component:AboutComponent
+      }
+
+    ])
   ],
   providers: [],
-  bootstrap: [HomeListComponent]
+  bootstrap: [FrameworkComponent]
 })
 export class AppModule { }
